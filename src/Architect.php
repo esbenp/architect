d2a1b34b564bdb73c65a4a154f0a07dda236a045
Patrick McCarren <patrick@mmsc.io>
2018-05-15 16:26:57 -0400

    fix: nested relations that match same model multiple times

--- src/Architect.php
+++ src/Architect.php
@@ -96,6 +96,14 @@ class Architect
                     continue;
                 }
 
+                // if we failed to load the relation and the resource is a
+                // string (the id), we can't do anything, so return
+                // FIXME: Find root cause of error "Indirect modification of overloaded property"
+                // (Note: encountered with relations 3 levels deep, with same model relating to multiple)
+                if ($mode === 'sideload' && getType($resource->{$property}) === 'string') {
+                    return;
+                }
+
                 $object = &$resource->{$property};
             }
 
